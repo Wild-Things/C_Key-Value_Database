2e0e38ccb103b39f7fb9074d7f5087d78926f621
Wild-Things <sharpeproduction@gmail.com>
2025-12-11 17:21:08 -0500

    Removed the compiled main file

--- src/main.c
+++ src/main.c
@@ -1,9 +1,55 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <getopt.h>
 
 #include "libfunction.h"
+#include "parse.h"
+#include "file.h"
 
-int main()
+void print_usage(char *argv[])
 {
+	printf("Usage: %s -n -f <database file>\n", argv[0]);	
+	printf("\t -n  - create new database file\n");
+	printf("\t -f  - [required] directory location of the database file\n");
+
+	return;
+}
+
+int main(int argc, char *argv[])
+{
+	char *filepath = NULL;
+	bool newfile = false;
+	int c;
+	while ((c = getopt(argc, argv, "nf:")) != -1)
+	{
+		switch(c)
+		{
+			case 'n':
+				newfile = true;
+				break;
+			case 'f':
+				filepath = optarg;
+				break;
+			case '?':
+				printf("Unknown option -%c\n", c);
+				break;
+			default:
+				return -1;	
+		}
+	}	
+
+	if (filepath == NULL)
+	{
+		printf("Filepath is a required argument\n");
+		print_usage(argv);
+
+		return 0;
+	}
+
+	
+	printf("New file: %d\n", newfile);
+	printf("file path: %s\n", filepath);
+
 	does_it_work();
 	return 0;
 }
